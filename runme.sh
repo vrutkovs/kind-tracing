f4b5dbf42707932021d7b452e1864aabd9b6c39e
Vadim Rutkovsky <vrutkovs@redhat.com>
2022-12-13 23:39:17 +0100

    Add custom kubelet/kubeapi
    This adds kind / kubeadm configs to use custom kind node/kube-apiserver
    images. Built binaries are put in `custom-kubeapi`/`custom-node` dirs and `runme` builds images in local registry before starting a cluster

--- runme.sh
+++ runme.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 set -eux
 
+# Run local registry
+if [ "$(podman inspect -f '{{.State.Running}}' "kind-registry" 2>/dev/null || true)" != 'true' ]; then
+  podman run \
+    -d --restart=always \
+    -p "127.0.0.1:5001:5000" \
+    --name "kind-registry" \
+    --network kind \
+    registry:2
+fi
+
+# Build custom images
+podman build -t localhost:5001/custom:kubeapi custom-kubeapi
+podman push localhost:5001/custom:kubeapi --tls-verify=false
+podman build -t localhost:5001/custom:kubelet custom-node
+podman push localhost:5001/custom:kubelet --tls-verify=false
+
+# Run kind cluster
 kind create cluster --config=./config.yaml
 kubectl apply -f grafana-agent
 kubectl get nodes
